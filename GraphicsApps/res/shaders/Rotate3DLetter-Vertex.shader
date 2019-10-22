b288e347bb770a086116394cc6f4456cbf627f1d
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2019-10-21 22:42:34 -0700

    CubePersp.cpp Chap 7 Exercise 7.6

--- GraphicsApps/res/shaders/Rotate3DLetter-Vertex.shader
+++ GraphicsApps/res/shaders/Rotate3DLetter-Vertex.shader
@@ -1,9 +1,9 @@
 #version 130								          
-in vec2 point;								          
+in vec3 point;								          
 in vec3 color;								          
 out vec4 vColor;							          
 uniform mat4 view;                                    
 void main() {
-	gl_Position = view * vec4(point, 0, 1);		      
+	gl_Position = view * vec4(point, 1);		      
 	vColor = vec4(color, 1);			              
 }
--- GraphicsApps/src/CubePersp.cpp
+++ GraphicsApps/src/CubePersp.cpp
@@ -6,6 +6,7 @@
 #include <VecMat.h>
 #include "GLXtras.h" 
 #include "Vertex.h"
+#include <stdlib.h>
 
 // GPU identifiers
 GLuint vBuffer = 0;  
@@ -13,27 +14,38 @@ GLuint program = 0;
 
 // user input handling
 float rotSpeed = .3f;               // deg rotation per #pixels dragged by mouse
-vec2 mouseDown(0, 0);               // location of last mouse down
-vec2 rotOld(0, 0), rotNew(0, 0);    // .x is rotation about Y-axis, in deg; .y about X-axis
-
+vec2 mouseDown(0, 0);               // location of last mouse down	 
+vec3 rotOld(0, 0, 0), rotNew(0, 0, 0);    // .x is rotation about Y-axis, in deg; .y about X-axis
+vec3 tranOld(0, 0,0), tranNew(0, 0,-1);	// old/new translate
+float tranSpeed = .01f; 
 void MouseButton(GLFWwindow* w, int butn, int action, int mods) {
     // called when mouse button pressed or released
     if (action == GLFW_PRESS) {
-        // save reference for MouseDrag
+        // save reference for MouseMove
         double x, y;
         glfwGetCursorPos(w, &x, &y);
         mouseDown = vec2((float)x, (float)y);
     }
-    if (action == GLFW_RELEASE)
-        // save reference rotation
-        rotOld = rotNew;
+	if (action == GLFW_RELEASE) {
+		// save reference rotation
+		rotOld = rotNew;
+		tranOld = tranNew;
+	}
 }
 void MouseMove(GLFWwindow* w, double x, double y) {
-    if (glfwGetMouseButton(w, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
-        // compute mouse drag difference, update rotation
-        vec2 dif((float)x - mouseDown.x, (float)y - mouseDown.y);
-        rotNew = rotOld + rotSpeed * dif;
+	if (glfwGetMouseButton(w, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+		vec2 mouse((float)x, (float)y);
+		vec2 dif = mouse - mouseDown;
+		rotNew = rotOld + rotSpeed * vec3(dif, 1);
+		//printf("MOUSEMOVE: RotNew X: %.0f Y: %.0f Z: %.0f\n", rotNew.x, rotNew.y, rotNew.z);
     }
+}
+// mouse wheel rotation
+static float degPerSec = 30;
+static double mouseWheelScalar = 2;
+void MouseWheel(GLFWwindow* w, double xoffset, double yoffset) {
+	tranNew.z += (yoffset + xoffset) * .25; //* mouseWheelScalar;
+	printf("MOUSEWHEEL: RotNew X: %.0f Y: %.0f Z: %.0f\n", rotNew.x, rotNew.y, rotNew.z);
 }
 void Keyboard(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (action == GLFW_PRESS && key == GLFW_KEY_ESCAPE)
@@ -41,47 +53,63 @@ void Keyboard(GLFWwindow* window, int key, int scancode, int action, int mods) {
 }
 
 // H-Letter design
-Vertex vertices[] = {
-    Vertex(-.5f,-.8f,  0, 0, 1), Vertex(-.8f,-.8f,  1, 0, 0), // V0, V1
-    Vertex(-.8f, .8f,  0, 0, 1), Vertex(-.5f, .8f,  0, 1, 0),
-    Vertex(-.5f, .13f, 0, 0, 1), Vertex( .5f, .13f, 0, 1, 0),
-    Vertex(-.5f,-.13f, 0, 0, 1), Vertex( .5f,-.13f, 1, 0, 0), // V6, V7
-    Vertex( .8f,-.8f,  0, 0, 1), Vertex( .5f,-.8f,  1, 0, 0),
-    Vertex( .5f, .8f,  0, 0, 1), Vertex( .8f, .8f,  0, 1, 0),
-    Vertex(-.8f,-.13f, 1, 0, 0), Vertex(-.8f, .13f, 0, 0, 1), // V12, 13
-    Vertex( .8f,-.13f, 1, 0, 0), Vertex( .8f, .13f, 0, 0, 1) 
-};
-int triangles[][3] = {
-    {0,  1, 12}, {2,  3,  4}, {4,  5,  6}, {5, 6,  7}, {8,  9, 7}, {10, 11, 15}, 
-    {0, 12,  6}, {2, 13,  4}, {6, 12, 13}, {6, 4, 13}, {8, 14, 7}, {10, 15,  5}, 
-    {7, 14, 15}, {7,  5, 15}
-};
+float l = -1, r = 1, b = -1, t = 1, n = -1, f = 1;
+float points[][3] = { {l,b,n},{l,b,f},{l,t,n},{l,t,f},{r,b,n},{r,b,f},{r,t,n},{r,t,f} };
+float colors[][3] = { {0,0,1},{0,1,0},{0,1,1},{1,0,0},{1,0,1},{1,1,0},{0,0,0},{1,1,1} };
+int faces[][4] = { {1,3,2,0},{6,7,5,4},{4,5,1,0},{3,7,6,2},{2,6,4,0},{5,7,3,1} };
+float fieldOfView = 30, cubeSize = .05f, cubeStretch = cubeSize;
 
-void Display() {
-    mat4 view = RotateY(rotNew.y) * RotateX(rotNew.x);
-    SetUniform(program, "view", view);
+
+void Display(GLFWwindow *w) {
+
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	//mat4 view = Translate(tranNew.x,tranNew.y, 0) * RotateY(rotNew.x) * RotateX(rotNew.y) * RotateZ(rotNew.z);
+
+	int screenWidth, screenHeight;
+	glfwGetWindowSize(w, &screenWidth, &screenHeight);
+
+	int halfWidth = screenWidth / 2;
+	float aspectRatio = (float)halfWidth / (float)screenHeight;
+	float nearDistance = .001f, farDistance = 500;
+	mat4 persp = Perspective(fieldOfView, aspectRatio, nearDistance, farDistance);
+
+	mat4 scale = Scale(cubeSize, cubeSize, cubeStretch);
+	mat4 rot = RotateY(rotNew.x) * RotateX(rotNew.y);
+	mat4 tran = Translate(tranNew);
+	mat4 modelview = tran * rot * scale;
+	mat4 view = persp * modelview;
+	SetUniform(program, "view", view);
+
     // clear to gray, use app's shader
     glClearColor(.5, .5, .5, 1);
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(program);
     // set vertex feed for points and colors, then draw
     glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
-    VertexAttribPointer(program, "point", 2, sizeof(Vertex), (void*) 0);
-    VertexAttribPointer(program, "color", 3, sizeof(Vertex), (void*) sizeof(vec2));
-    glDrawElements(GL_TRIANGLES, 42, GL_UNSIGNED_INT, triangles);
-    glFlush();
+	VertexAttribPointer(program, "point", 3, 0, (void*)0);
+	VertexAttribPointer(program, "color", 3, 0, (void*)0);
+	//VertexAttribPointer(program, "point", 2, sizeof(Vertex), (void*) 0);
+    
+	glViewport(0, 0, halfWidth, screenHeight);
+	glDrawElements(GL_QUADS, sizeof(faces) / sizeof(int), GL_UNSIGNED_INT, faces);
+	glViewport(halfWidth, 0, halfWidth, screenHeight);
+	glLineWidth(5);
+	for (int i = 0; i < 6; i++)
+		glDrawElements(GL_LINE_LOOP, 4, GL_UNSIGNED_INT, &faces[i]);
+	glFlush();
 }
 
 void InitVertexBuffer() {
     // make GPU buffer for points & colors, set it active buffer
     glGenBuffers(1, &vBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 }
 
 bool InitShader() { 
-    program = LinkProgramViaFile("res/shaders/Rotate3DLetter_Vertex.shader",
-                                 "res/shaders/Rotate3DLetter_Pixel.shader");
+    program = LinkProgramViaFile("res/shaders/Rotate3DLetter-Vertex.shader",
+                                 "res/shaders/Rotate3DLetter-Pixel.shader");
     if (!program)
         printf("can't init shader program\n");
     return program != 0;
@@ -101,13 +129,14 @@ int main() {
     glfwSetErrorCallback(ErrorGFLW);
     if (!glfwInit())
         return 1;
-    GLFWwindow *window = glfwCreateWindow(600, 600, "Colorful Letter", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(600, 600, "Rotate3DLetter", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return 1;
     }
     glfwSetMouseButtonCallback(window, MouseButton);
     glfwSetCursorPosCallback(window, MouseMove);
+	glfwSetScrollCallback(window, MouseWheel);
     glfwSetKeyCallback(window, Keyboard);
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
@@ -117,7 +146,7 @@ int main() {
         return 0;
     InitVertexBuffer();
     while (!glfwWindowShouldClose(window)) {
-        Display();
+        Display(window);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
